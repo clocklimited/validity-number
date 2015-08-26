d863f16ce6174e31e047bc113dbf230b32671dab
Matthew Elphick <matthew.elphick@clock.co.uk>
2015-08-26 13:30:43 +0100

    > rename tests to reflect integers/floats are allowed

--- test/number-validator.test.js
+++ test/number-validator.test.js
@@ -1,65 +1,65 @@
 var assert = require('assert')
-  , floatValidator = require('../validator')
+  , numberValidator = require('../validator')
 
-describe('Float Validator', function () {
+describe('Number Validator', function () {
   it('should allow floats', function (done) {
-    floatValidator('value', 'Value', { value: 10.5 }, function (err, errors) {
+    numberValidator('value', 'Value', { value: 10.5 }, function (err, errors) {
       assert.equal(errors, undefined, 'An error was outputted')
       done()
     })
   })
   
   it('should allow float strings', function (done) {
-    floatValidator('value', 'Value', { value: '10.5' }, function (err, errors) {
+    numberValidator('value', 'Value', { value: '10.5' }, function (err, errors) {
       assert.equal(errors, undefined, 'An error was outputted')
       done()
     })
   })
 
   it('should allow integers', function (done) {
-    floatValidator('value', 'Value', { value: 10 }, function (err, errors) {
+    numberValidator('value', 'Value', { value: 10 }, function (err, errors) {
       assert.equal(errors, undefined, 'An error was outputted')
       done()
     })
   })
 
   it('should allow integer strings', function (done) {
-    floatValidator('value', 'Value', { value: '10' }, function (err, errors) {
+    numberValidator('value', 'Value', { value: '10' }, function (err, errors) {
       assert.equal(errors, undefined, 'An error was outputted')
       done()
     })
   })
 
   it('should output an error for a string', function (done) {
-    floatValidator('value', 'Value', { value: 'test' }, function (err, errors) {
+    numberValidator('value', 'Value', { value: 'test' }, function (err, errors) {
       assert.equal(errors, 'Value must be a float', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for an array', function (done) {
-    floatValidator('value', 'Value', { value: [] }, function (err, errors) {
+    numberValidator('value', 'Value', { value: [] }, function (err, errors) {
       assert.equal(errors, 'Value must be a float', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for an object', function (done) {
-    floatValidator('value', 'Value', { value: {} }, function (err, errors) {
+    numberValidator('value', 'Value', { value: {} }, function (err, errors) {
       assert.equal(errors, 'Value must be a float', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for a null', function (done) {
-    floatValidator('value', 'Value', { value: null }, function (err, errors) {
+    numberValidator('value', 'Value', { value: null }, function (err, errors) {
       assert.equal(errors, 'Value must be a float', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for undefined', function (done) {
-    floatValidator('value', 'Value', { value: undefined }, function (err, errors) {
+    numberValidator('value', 'Value', { value: undefined }, function (err, errors) {
       assert.equal(errors, 'Value must be a float', 'No error was outputted')
       done()
     })

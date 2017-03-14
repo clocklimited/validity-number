622b9fad4f8d912e817095874d67653b4f290eb4
Dominick Painter <dominick.painter@clock.co.uk>
2017-03-14 14:13:01 +0000

    add check if NaN

--- test/number-validator.test.js
+++ test/number-validator.test.js
@@ -8,7 +8,7 @@ describe('Number Validator', function () {
       done()
     })
   })
-  
+
   it('should allow float strings', function (done) {
     numberValidator('value', 'Value', { value: '10.5' }, function (err, errors) {
       assert.equal(errors, undefined, 'An error was outputted')
@@ -32,36 +32,44 @@ describe('Number Validator', function () {
 
   it('should output an error for a string', function (done) {
     numberValidator('value', 'Value', { value: 'test' }, function (err, errors) {
-      assert.equal(errors, 'Value must be a float', 'No error was outputted')
+      assert.equal(errors, 'Value must be a number', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for an array', function (done) {
     numberValidator('value', 'Value', { value: [] }, function (err, errors) {
-      assert.equal(errors, 'Value must be a float', 'No error was outputted')
+      assert.equal(errors, 'Value must be a number', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for an object', function (done) {
     numberValidator('value', 'Value', { value: {} }, function (err, errors) {
-      assert.equal(errors, 'Value must be a float', 'No error was outputted')
+      assert.equal(errors, 'Value must be a number', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for a null', function (done) {
     numberValidator('value', 'Value', { value: null }, function (err, errors) {
-      assert.equal(errors, 'Value must be a float', 'No error was outputted')
+      assert.equal(errors, 'Value must be a number', 'No error was outputted')
       done()
     })
   })
 
   it('should output an error for undefined', function (done) {
     numberValidator('value', 'Value', { value: undefined }, function (err, errors) {
-      assert.equal(errors, 'Value must be a float', 'No error was outputted')
+      assert.equal(errors, 'Value must be a number', 'No error was outputted')
       done()
     })
   })
+
+  it('should output an error for NaN', function (done) {
+    numberValidator('value', 'Value', { value: NaN }, function (err, errors) {
+      assert.equal(errors, 'Value must be a number', 'No error was outputted')
+      done()
+    })
+  })
+
 })
